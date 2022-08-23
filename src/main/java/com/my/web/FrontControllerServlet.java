61c58ccf58898f3e60de985d1b498dfa57e4aabb
Konstantin <titarenko42k@gmail.com>
2022-08-23 22:56:39 +0300

    add base files

---
+++ src/main/java/com/my/web/FrontControllerServlet.java
@@ -0,0 +1,19 @@
+package com.my.web;
+
+
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "FrontControllerServlet", value = "/")
+public class FrontControllerServlet extends HttpServlet {
+
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    public void destroy() {
+    }
+}
