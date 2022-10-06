52c0d1744d703af5382446c4ef54e374492750c1
Konstantin <titarenko42k@gmail.com>
2022-10-06 21:01:59 +0300

    clean up code

--- src/main/java/com/my/persistence/db/ManagerDB.java
+++ src/main/java/com/my/persistence/db/ManagerDB.java
@@ -1,30 +1,14 @@
 package com.my.persistence.db;
 
-import com.my.annotations.Value;
-import com.my.exception.ManagerDBException;
 import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ManagerDB {
 
     private final static Logger log = Logger.getLogger(ManagerDB.class);
-
-    @Value(param = "db.driver")
-    private String driver;
-
-    @Value(param = "db.url")
-    private String url;
-
-    @Value(param = "db.user")
-    private  String user;
-
-    @Value(param = "db.password")
-    private String password;
-
 
     private static final String USER = "root";
     private static final String PASSWORD = "qwertyuiopOblt1>?";
@@ -58,13 +42,6 @@ public class ManagerDB {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(URL, USER, PASSWORD);
 
-            //log.info("url = " + url + ", driver = " + driver + ", user" + user);
-
-            /*Class.forName(driver);
-            con = DriverManager.getConnection(url, user, password);
-            if(con == null){
-                throw new ManagerDBException("Unable to get connection to the server");
-            }*/
             con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             con.setAutoCommit(false);
         } catch (ClassNotFoundException e) {
